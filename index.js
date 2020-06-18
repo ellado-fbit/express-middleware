07678727c29e0f1f23afc4faeb7e82396c2f97bd
Esteve Llado <ellado@fundaciobit.org>
2020-06-18 10:05:48 +0200

    Removed some middlewares moved to another repo

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 'use strict'
 
 const ipv4 = require('./src/ipv4/ipv4')
-const redisGet = require('./src/redis/redisGet')
-const redisSet = require('./src/redis/redisSet')
 
 module.exports = {
-  ipv4: ipv4,
-  redisGet: redisGet,
-  redisSet: redisSet
+  ipv4: ipv4
 }
